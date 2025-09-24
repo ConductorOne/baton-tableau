eaf4590c49e3bf0e6f3b6d85cc89172f52714b70
Luisina Santos <luisina.santos@conductorone.com>
2025-09-24 20:50:41 -0300

    Containerize connector

--- cmd/baton-tableau/main.go
+++ cmd/baton-tableau/main.go
@@ -25,7 +25,7 @@ func main() {
 	_, cmd, err := config.DefineConfiguration(
 		ctx,
 		"baton-tableau",
-		getConnector[*cfg.Tableau],
+		getConnector,
 		cfg.Config,
 	)
 	if err != nil {
@@ -42,18 +42,18 @@ func main() {
 	}
 }
 
-func getConnector[T field.Configurable](ctx context.Context, config T) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, tc *cfg.Tableau) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
-	if err := field.Validate(cfg.Config, config); err != nil {
+	if err := field.Validate(cfg.Config, tc); err != nil {
 		return nil, err
 	}
 
-	apiVersion := config.GetString(cfg.APIVersion.FieldName)
+	apiVersion := tc.ApiVersion
 	if apiVersion == "" {
 		apiVersion = "3.17"
 	}
 
-	serverPath := config.GetString(cfg.ServerPath.FieldName)
+	serverPath := tc.ServerPath
 	baseUrl, err := url.JoinPath("https://", serverPath, "api", apiVersion)
 	if err != nil {
 		l.Error("error creating base url", zap.Error(err))
@@ -62,9 +62,9 @@ func getConnector[T field.Configurable](ctx context.Context, config T) (types.Co
 	cb, err := connector.New(
 		ctx,
 		baseUrl,
-		config.GetString(cfg.SiteID.FieldName),
-		config.GetString(cfg.AccessTokenName.FieldName),
-		config.GetString(cfg.AccessTokenSecret.FieldName),
+		tc.SiteId,
+		tc.AccessTokenName,
+		tc.AccessTokenSecret,
 	)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
