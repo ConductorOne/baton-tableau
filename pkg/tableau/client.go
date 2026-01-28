73a66c5993495eee38432de90b6225a3c433e2e8
Alejandro Bernal <jbernalxyz@gmail.com>
2026-01-28 11:36:13 -0500

    remove dead code

--- pkg/tableau/client.go
+++ pkg/tableau/client.go
@@ -407,8 +407,6 @@ func (c *Client) AddUserToSite(ctx context.Context, user CreateUserRequest) (*Us
 
 	if user.IdpConfigurationId != "" {
 		userMap["idpConfigurationId"] = user.IdpConfigurationId
-	} else if user.AuthSetting != "" {
-		userMap["authSetting"] = user.AuthSetting
 	}
 
 	requestBody, err := json.Marshal(map[string]interface{}{
